b7f336e863e85958c26658c813042c1f461495df
Dean Lonergan <74914758+DeanLonergan@users.noreply.github.com>
2023-09-28 17:42:52 +0100

    Demo optional properties and parameters

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -36,11 +36,17 @@ function addColleague(cs: Colleague[], name: string, department: string, email: 
 
 function sortColleagues(
     colleagues: Colleague[],
-    sorter: (c1: Colleague, c2: Colleague) => number): EmailContact[] {
-    const sorted = colleagues.sort(sorter); // Colleague[] inferred
-    const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
-    return result 
-}
+    sorter: (c1: Colleague, c2: Colleague) => number,
+    max? : number
+  ): EmailContact[] {
+    let end = colleagues.length;
+    if (max !== undefined) {
+       end = max < 2 ? 1 : max
+    }
+    const sorted = colleagues.sort(sorter);
+    const fullResult =  sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+    return fullResult.slice(0,end)
+  }
 
 function findFriends(f: Friend[], 
     filter: (f: Friend) => boolean) {
@@ -54,5 +60,8 @@ function findFriends(f: Friend[],
 // console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 // console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
 // console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
-console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
-console.log(findFriends(friends, (friend) => friend.age < 35));
+// console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
+// console.log(findFriends(friends, (friend) => friend.age < 35));
+console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
+console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
+console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length)));
